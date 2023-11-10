1a1187f4f12cac4a7f35ee0752c293bdb6cd8ecd
Uday Varada <uvarada@thxms.theradex.com>
2023-11-09 18:28:18 -0800

    Fixed dockerfile for 7.0 framework...

--- src/push-image.bat
+++ src/push-image.bat
@@ -1,19 +1,4 @@
-@ECHO Build for TAG %1%
-@ECHO if you did not specify valid TAG then Ctrl+C and try again
-pause
-
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 993530973844.dkr.ecr.us-east-1.amazonaws.com
-@ECHO Any errors Ctrl+C and do something to fix
-pause 
-
-docker  build --build-arg Version=%1% -f "ods.extractor/Dockerfile" -t odsextractor:%1% .
-@ECHO Any errors Ctrl+C and do something to fix
-pause
-
-#docker tag odsextractor:latest 993530973844.dkr.ecr.us-east-1.amazonaws.com/odsextractor:%1%
-#@ECHO Any errors Ctrl+C and do something to fix
-#pause
-
-#docker push 993530973844.dkr.ecr.us-east-1.amazonaws.com/odsextractor:%1%
-#@ECHO Any errors Ctrl+C and do something to fix
-#pause
+ods\src> docker build -f "ods.extractor/Dockerfile" -t dev-theradexodsextractor . 
+docker tag dev-theradexodsextractor:latest 993530973844.dkr.ecr.us-east-1.amazonaws.com/dev-theradexodsextractor:dev-latest
+docker push 993530973844.dkr.ecr.us-east-1.amazonaws.com/dev-theradexodsextractor:dev-latest
