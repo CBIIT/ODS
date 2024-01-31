09ec68a7dd7a066fb52479d6741870197234e96e
Manish Rathi <mgrathi.99@gmail.com>
2024-01-31 05:29:52 -0800

    Fixed docker file and bat

--- src/ods.manager/Tag-Push.bat
+++ src/ods.manager/Tag-Push.bat
@@ -2,6 +2,6 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 
 ods\src > docker build -f "ods.manager/Dockerfile" -t dev-theradexodsmanager . 
 
-docker tag dev-theradexodsManager:latest 993530973844.dkr.ecr.us-east-1.amazonaws.com/dev-theradexodsmanager:dev-latest
+docker tag dev-theradexodsmanager:latest 993530973844.dkr.ecr.us-east-1.amazonaws.com/dev-theradexodsmanager:dev-latest
 
 docker push 993530973844.dkr.ecr.us-east-1.amazonaws.com/dev-theradexodsmanager:dev-latest
